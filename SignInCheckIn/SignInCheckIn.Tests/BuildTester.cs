9c3cf4ecbe23aef4d609dd11ed50774d7ed6e396
Matt Brewer <matt.brewer@gmail.com>
2016-04-29 11:45:43 -0400

    Adding test project to test TeamCity build

---
+++ SignInCheckIn/SignInCheckIn.Tests/BuildTester.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SignInCheckIn.Tests
+{
+    [TestFixture]
+    public class BuildTester
+    {
+        [Test]
+        public void ShouldPass()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        [Category("IntegrationTests")]
+        public void ShouldPassLocallyButNotRunOnBuildServer()
+        {
+            Assert.Pass();
+        }
+    }
+}
