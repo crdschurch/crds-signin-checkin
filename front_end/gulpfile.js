8b62d13b8cceee25e54cd3fa80d9977b38df7771
Brian Miller <bzmiller82@gmail.com>
2016-05-19 22:57:27 -0400

    SASS and Gulp Setup
    US3890

---
+++ front_end/gulpfile.js
@@ -0,0 +1,30 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+var prefix      = require('gulp-autoprefixer');
+var plumber     = require ('gulp-plumber');
+
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "dist/"
+    });
+
+    gulp.watch("scss/*.scss", ['sass']);
+    gulp.watch("**/*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("scss/*.scss")
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(gulp.dest("dist/css"))
+        .pipe(browserSync.stream())
+        .pipe(gulp.dest("src/css"));
+});
+
+gulp.task('default', ['serve']);
--- front_end/src/app/front-end.component.ts
+++ front_end/src/app/front-end.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['front-end.component.css']
 })
 export class FrontEndAppComponent {
-  title = 'front-end works!';
+  title = 'The Angular 2 app works!';
 }
