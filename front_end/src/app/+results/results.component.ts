b4a2aeed72a1a14e4c23db2e7096c56bc1a1799c
tatelucas <tate.lucas@gmail.com>
2016-06-01 16:05:58 -0400

    US4081 enhancements

--- front_end/src/app/+results/results.component.ts
+++ front_end/src/app/+results/results.component.ts
@@ -18,6 +18,17 @@ import { Routes , ROUTER_DIRECTIVES} from '@angular/router';
 ])
 export class ResultsComponent implements OnInit {
 
+  private cb1: boolean = true;
+  private cb2: boolean = true;
+  private cb3: boolean = false;
+  private cb4: boolean = true;
+  private cb5: boolean = true;
+  private cb6: boolean = true;
+  private cb7: boolean = true;
+  private cb8: boolean = true;
+  private cb9: boolean = true;
+  private cb10: boolean = true;
+
   constructor() {}
 
   ngOnInit() {
