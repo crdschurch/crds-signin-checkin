51889b4887f701e16d48d5b29126f5b8762d6c6b
Jim Kriz <jim.kriz@ingagepartners.com>
2016-09-28 09:50:56 -0400

    US1878: Versioning implementation

--- SignInCheckIn/SignInCheckIn/App_Start/WebApiConfig.cs
+++ SignInCheckIn/SignInCheckIn/App_Start/WebApiConfig.cs
@@ -1,26 +1,22 @@
 ﻿using System.Web.Http;
-using SDammann.WebApi.Versioning.Configuration;
-using SDammann.WebApi.Versioning.Request;
+using SignInCheckIn.ApiVersioning;
+using SignInCheckIn.ApiVersioning.Filters;
 
 namespace SignInCheckIn
 {
     public static class WebApiConfig
     {
+        public static ApiRouteProvider ApiRouteProvider = new ApiRouteProvider();
+
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
 
-            // Web API routes
-            config.MapHttpAttributeRoutes();
+            // Web API routes - use a custom route provider to prepend "api/" on all routes
+            config.MapHttpAttributeRoutes(ApiRouteProvider);
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
-
-            ApiVersioning.Configure(config)
-             .ConfigureRequestVersionDetector<DefaultRouteKeyVersionDetector>();
+            // Add a filter to change 200 to 299 response code for deprecated APIs
+            config.Filters.Add(new DeprecatedVersionFilter());
         }
     }
 }
---
+++ SignInCheckIn/SignInCheckIn/Services/HelloWorldService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using SignInCheckIn.Models;
+using SignInCheckIn.Services.Interfaces;
+
+namespace SignInCheckIn.Services
+{
+    public class HelloWorldService : IHelloWorldService
+    {
+        public HelloWorldOutputDto Greet(HelloWorldInputDto input)
+        {
+            // V1
+            if (input.GetType() == typeof (HelloWorldInputDto))
+            {
+                return new HelloWorldOutputDto
+                {
+                    Message = $"Hello {input.Name}!",
+                    Name = input.Name
+                };
+            }
+
+            // V2
+            if (input.GetType() == typeof (HelloWorldV2InputDto))
+            {
+                var v2 = (HelloWorldV2InputDto) input;
+                return new HelloWorldV2OutputDto
+                {
+                    Message = $"Hello {v2.FirstName} {v2.LastName}!",
+                    FirstName = v2.FirstName,
+                    LastName = v2.LastName
+                };
+            }
+
+            throw new InvalidOperationException("API version not supported");
+        }
+    }
+}
