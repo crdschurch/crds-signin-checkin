196c7fe4f190ddaf5f981b2c78b486bc6f491f69
DaveAnderson <eymiha@gmail.com>
2016-10-06 13:40:12 -0400

    simplify parser logic

--- SignInCheckIn/SignInCheckIn/ApiVersioning/SemanticVersion.cs
+++ SignInCheckIn/SignInCheckIn/ApiVersioning/SemanticVersion.cs
@@ -5,22 +5,20 @@ namespace SignInCheckIn.ApiVersioning
 {
     public class SemanticVersion : IComparable<SemanticVersion>
     {
-        private const string SemanticVersionRegexPatterm = @"^(\d+)?\.?(\d+)?\.?(\d+)?$";
+        private const string SemanticVersionRegexPatterm = @"^(\d+)\.(\d+)\.(\d+)";
         private readonly Regex _semanticVersionRegex = new Regex(SemanticVersionRegexPatterm);
         public Version Version { get; }
         public SemanticVersion(string version)
         {
-            var match = _semanticVersionRegex.Match(version);
-            if (!match.Success || match.Groups.Count <= 2 || !match.Groups[1].Success)
+            var match = _semanticVersionRegex.Match(version + ".0.0");
+            if (!match.Success || match.Groups.Count != 4)
             {
                 throw new ArgumentException($"Version provided ({version}) is not valid");
             }
-
             var groups = match.Groups;
             var major = int.Parse(groups[1].Captures[0].Value);
-            var minor = groups.Count >= 3 && groups[2].Success ? int.Parse(groups[2].Captures[0].Value) : 0;
-            var patch = groups.Count >= 4 && groups[3].Success ? int.Parse(groups[3].Captures[0].Value) : 0;
-
+            var minor = int.Parse(groups[2].Captures[0].Value);
+            var patch = int.Parse(groups[3].Captures[0].Value);
             Version = new Version(major, minor, patch);
         }
 
@@ -67,16 +65,11 @@ namespace SignInCheckIn.ApiVersioning
         public int CompareTo(SemanticVersion other)
         {
             if (other == null || IsAfter(other))
-            {
                 return 1;
-            }
-
-            if (IsBefore(other))
-            {
+            else if (IsBefore(other))
                 return -1;
-            }
-
-            return 0;
+            else
+                return 0;
         }
 
         public override string ToString()
