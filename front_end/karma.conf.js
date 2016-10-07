255b9692dcb7d5b3c3144d39e795608d0763953c
Jim Kriz <jim.kriz@ingagepartners.com>
2016-10-07 08:48:54 -0400

    US5169: Update karma conf to report properly

--- front_end/karma.conf.js
+++ front_end/karma.conf.js
@@ -10,6 +10,7 @@ module.exports = function (config) {
       require('karma-phantomjs-launcher'),
       require('karma-teamcity-reporter'),
       require('karma-remap-istanbul'),
+      require('karma-mocha-reporter'),
       require('angular-cli/plugins/karma')
     ],
     files: [
@@ -28,12 +29,19 @@ module.exports = function (config) {
       config: './angular-cli.json',
       environment: 'dev'
     },
-    reporters: ['progress', 'karma-remap-istanbul'],
+    // reporter(s) to use - set to 'teamcity' for ci server
+    reporters: ['mocha'],
     port: 9876,
+    // enable / disable colors in the output (reporters and logs)
     colors: true,
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
+    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
     browsers: ['PhantomJS'],
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits; set to true for ci server
     singleRun: false
   });
 };
