aa0fbb55ec78d7c3607a2907566433d7ab238878
Jim Kriz <jim.kriz@ingagepartners.com>
2016-10-10 15:04:25 -0400

    US5283: Get log4net working

---
+++ SignInCheckIn/SignInCheckIn/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignInCheckIn.Startup))]
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config", Watch = true)]
+namespace SignInCheckIn
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
