ef6f1e6412037d3c942e2ad607be4cbd814875ff
Tim Giblin <timmyg13@gmail.com>
2016-10-11 14:19:57 -0400

    hello world test passing now, still some errors

--- front_end/config/karma.conf.js
+++ front_end/config/karma.conf.js
@@ -1,44 +1,105 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/0.13/config/configuration-file.html
+/**
+ * @author: @AngularClass
+ */
 
-module.exports = function (config) {
-  config.set({
-    basePath: './src',
+module.exports = function(config) {
+  var testWebpackConfig = require('./webpack.test.js')({env: 'test'});
+
+  var configuration = {
+
+    // base path that will be used to resolve all patterns (e.g. files, exclude)
+    basePath: '',
+
+    /*
+     * Frameworks to use
+     *
+     * available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+     */
     frameworks: ['jasmine'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-phantomjs-launcher'),
-      require('karma-teamcity-reporter'),
-      require('karma-remap-istanbul'),
-      require('karma-mocha-reporter'),
-      require('karma-webpack'),
-      require('karma-sourcemap-loader')
-    ],
-    files: [
-      { pattern: './test.js', watched: false }
-    ],
-    preprocessors: {
-      './karma-test-shim.js': ['webpack', 'sourcemap']
+
+    // list of files to exclude
+    exclude: [ ],
+
+    /*
+     * list of files / patterns to load in the browser
+     *
+     * we are building the test environment in ./spec-bundle.js
+     */
+    files: [ { pattern: './config/spec-bundle.js', watched: false } ],
+
+    /*
+     * preprocess matching files before serving them to the browser
+     * available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+     */
+    preprocessors: { './config/spec-bundle.js': ['coverage', 'webpack', 'sourcemap'] },
+
+    // Webpack Config at ./webpack.test.js
+    webpack: testWebpackConfig,
+
+    coverageReporter: {
+      type: 'in-memory'
     },
-    remapIstanbulReporter: {
-      reports: {
-        html: 'coverage',
-        lcovonly: './coverage/coverage.lcov'
-      }
+
+    remapCoverageReporter: {
+      'text-summary': null,
+      json: './coverage/coverage.json',
+      html: './coverage/html'
     },
-    // reporter(s) to use - set to 'teamcity' for ci server
-    reporters: ['mocha'],
+
+    // Webpack please don't spam the console when running in karma!
+    webpackMiddleware: { stats: 'errors-only'},
+
+    /*
+     * test results reporter to use
+     *
+     * possible values: 'dots', 'progress'
+     * available reporters: https://npmjs.org/browse/keyword/karma-reporter
+     */
+    reporters: [ 'mocha', 'coverage', 'remap-coverage' ],
+
+    // web server port
     port: 9876,
+
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+
+    /*
+     * level of logging
+     * possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+     */
     logLevel: config.LOG_INFO,
+
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-    browsers: ['PhantomJS'],
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits; set to true for ci server
-    singleRun: false
-  });
+    autoWatch: false,
+
+    /*
+     * start these browsers
+     * available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+     */
+    browsers: [
+      'PhantomJS'
+    ],
+
+    customLaunchers: {
+      ChromeTravisCi: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
+
+    /*
+     * Continuous Integration mode
+     * if true, Karma captures browsers, runs the tests and exits
+     */
+    singleRun: true
+  };
+
+  if (process.env.TRAVIS){
+    configuration.browsers = [
+      'ChromeTravisCi',
+      'PhantomJS'
+    ];
+  }
+
+  config.set(configuration);
 };
