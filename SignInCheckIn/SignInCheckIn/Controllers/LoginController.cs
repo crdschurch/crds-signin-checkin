3d78d1d8fa4f1c0a6b1e51efbc5043ae7a0efeeb
jpc11623 <john.cleaver@ingagepartners.com>
2016-10-11 10:34:09 -0400

    Added logging to controller and uncommented configwrapper

--- SignInCheckIn/SignInCheckIn/Controllers/LoginController.cs
+++ SignInCheckIn/SignInCheckIn/Controllers/LoginController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Reflection;
 using System.Web.Http;
 using System.Web.Http.Description;
+using log4net;
 using SignInCheckIn.Models.Authentication;
 using SignInCheckIn.Services.Interfaces;
 
@@ -9,6 +11,7 @@ namespace SignInCheckIn.Controllers
     public class LoginController : ApiController
     {
         private readonly ILoginService _loginService;
+        protected readonly log4net.ILog logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
         public LoginController(ILoginService loginService)
         {
@@ -32,6 +35,8 @@ namespace SignInCheckIn.Controllers
                 {
                     return Unauthorized();
                 }
+
+                logger.Error("Error authenticating user", e);
 
                 return InternalServerError();                
             }
