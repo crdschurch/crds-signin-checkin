85d65806295c3f8ab662eee95944607a2ffd780a
jpc11623 <john.cleaver@ingagepartners.com>
2016-10-12 08:42:56 -0400

    Initial Commit
    US3508 - View List of Events

---
+++ SignInCheckIn/SignInCheckIn/Models/Json/EventSearch.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignInCheckIn.Models.Json
+{
+    public class EventSearch
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public List<int> EventTypeIds { get; set; } 
+    }
+}
---
+++ SignInCheckIn/SignInCheckIn/Services/EventService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MinistryPlatform.Translation.Repositories.Interfaces;
+using SignInCheckIn.Models.DTO;
+using SignInCheckIn.Models.Json;
+using SignInCheckIn.Services.Interfaces;
+
+namespace SignInCheckIn.Services
+{
+    public class EventService : IEventService
+    {
+        private readonly IEventRepository _eventRepository;
+
+        public EventService(IEventRepository eventRepository)
+        {
+            _eventRepository = eventRepository;
+        }
+
+        public List<EventDto> GetCheckinEvents()
+        {
+            var startDate = DateTime.Now;
+            var endDate = DateTime.Now.AddDays(7);
+            List<int> serviceTypeIds = new List<int>();
+            serviceTypeIds.Add(1);
+
+            var mpEvents = _eventRepository.GetEvents(startDate, endDate, serviceTypeIds);
+            List<EventDto> returnEvents = new List<EventDto>();
+
+            foreach (var eventItem in mpEvents)
+            {
+                returnEvents.Add(new EventDto
+                {
+                    EventId = eventItem.EventId,
+                    EventTitle = eventItem.EventTitle,
+                    EventStartDate = eventItem.EventStartDate          
+                });
+            }
+            
+            return returnEvents;
+        }
+    }
+}
