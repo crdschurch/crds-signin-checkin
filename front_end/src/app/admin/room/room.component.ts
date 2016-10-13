def74050a08cacba6ef0c358e9fb8c2ce6244ec8
Tim Giblin <timmyg13@gmail.com>
2016-10-13 16:17:13 -0400

    US3508: remove dummy data, comment out code for another US

--- front_end/src/app/admin/room/room.component.ts
+++ front_end/src/app/admin/room/room.component.ts
@@ -8,22 +8,22 @@ import { AdminService } from '../admin.service';
   styleUrls: ['room.component.scss']
 })
 export class RoomComponent {
-  room: any;
-
-  constructor(
-    private route: ActivatedRoute,
-    private adminService: AdminService,
-    private service: AdminService) {}
-
-  private getData(): void {
-    const eventId = this.route.snapshot.params['eventId'];
-    const roomId = this.route.snapshot.params['roomId'];
-    this.adminService.getRoom(eventId, roomId).subscribe(
-      room => {this.room = room},
-      error => console.error(error)
-    );
-  }
-  ngOnInit(): void {
-    this.getData();
-  }
+  // room: any;
+  //
+  // constructor(
+  //   private route: ActivatedRoute,
+  //   private adminService: AdminService,
+  //   private service: AdminService) {}
+  //
+  // private getData(): void {
+  //   const eventId = this.route.snapshot.params['eventId'];
+  //   const roomId = this.route.snapshot.params['roomId'];
+  //   this.adminService.getRoom(eventId, roomId).subscribe(
+  //     room => {this.room = room},
+  //     error => console.error(error)
+  //   );
+  // }
+  // ngOnInit(): void {
+  //   this.getData();
+  // }
 }
