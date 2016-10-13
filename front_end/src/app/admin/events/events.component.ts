5d931b81976e4e0553cda372a6da3878d0f538ba
Tim Giblin <timmyg13@gmail.com>
2016-10-13 08:36:31 -0400

    pull remote data on events, rooms, room components

--- front_end/src/app/admin/events/events.component.ts
+++ front_end/src/app/admin/events/events.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 import { AdminService } from '../admin.service';
 
 @Component({
--- front_end/src/app/admin/room/room.component.ts
+++ front_end/src/app/admin/room/room.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { AdminService } from '../admin.service';
 
 @Component({
   selector: 'room',
@@ -6,4 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['room.component.scss']
 })
 export class RoomComponent {
+  room: any;
+
+  constructor(
+    private route: ActivatedRoute,
+    private adminService: AdminService,
+    private service: AdminService) {}
+
+  private getData(): void {
+    const eventId = this.route.snapshot.params['eventId'];
+    const roomId = this.route.snapshot.params['roomId'];
+    this.adminService.getRoom(eventId, roomId).subscribe(
+      room => {this.room = room},
+      error => console.error(error)
+    );
+  }
+  ngOnInit(): void {
+    this.getData();
+  }
 }
