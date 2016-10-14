497e083b577009a0ff29a48283a535760617c6b7
Jim Kriz <jim.kriz@ingagepartners.com>
2016-10-13 23:39:26 -0400

    US5176: Remove unused imports

--- SignInCheckIn/SignInCheckIn.Tests/Services/RoomServiceTest.cs
+++ SignInCheckIn/SignInCheckIn.Tests/Services/RoomServiceTest.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using AutoMapper;
-using Castle.Components.DictionaryAdapter;
+﻿using System.Collections.Generic;
 using MinistryPlatform.Translation.Models.DTO;
 using MinistryPlatform.Translation.Repositories.Interfaces;
 using Moq;
