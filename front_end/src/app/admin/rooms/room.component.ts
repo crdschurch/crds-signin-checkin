4dbe858494d8f72ba20a66e3cfa4e64cfd7d8431
Tim Giblin <timmyg13@gmail.com>
2016-10-20 17:14:01 -0400

    DE2056: volunteers debouncing working

--- front_end/src/app/admin/rooms/room.component.ts
+++ front_end/src/app/admin/rooms/room.component.ts
@@ -11,37 +11,25 @@ import { Room } from './room';
 })
 export class RoomComponent {
   @Input() room: Room;
-  term = new FormControl();
+  volunteers = new FormControl();
 
   constructor(
     private adminService: AdminService) {
-      this.term.valueChanges
-         .debounceTime(400)
-         .subscribe(term => {
-           console.log("updateRoom", this)
-           this.adminService.updateRoom(this.room.EventId, this.room.RoomId, this.room).then(val => {
-             console.log("val", val);
-           })
-         });
+     this.volunteers.valueChanges
+        .debounceTime(1000)
+        .subscribe(props => {
+          this.adminService.updateRoom(this.room.EventId, this.room.RoomId, this.room).subscribe(val => {
+            console.log("val", val);
+          })
+        });
     }
-  //
-  // private getData(): void {
-  //   const eventId = this.route.snapshot.params['eventId'];
-  //   const roomId = this.route.snapshot.params['roomId'];
-  //   this.adminService.getRoom(eventId, roomId).subscribe(
-  //     room => {this.room = room},
-  //     error => console.error(error)
-  //   );
-  // }
+
   ngOnInit(): void {
-    // this.getData();
     console.log("room component oninit", this)
   }
 
-  addVolunteer(): void {
-    console.log("addVolunteer", this.room)
-    // room.Volunteers++
-    // this.adminService.updateRoom(room.EventId, room.RoomId, room).subscribe((room: Room) => { room = room },
-    //   (error: any) => {});
-  }
+  addVolunteer(): void { this.volunteers.setValue(this.room.Volunteers++) }
+  removeVolunteer(): void { this.volunteers.setValue(this.room.Volunteers--) }
+  // addCapacity(): void { this.props.setValue(this.room.Capacity++) }
+  // removeCapacity(): void { this.props.setValue(this.room.Capacity--) }
 }
