a5d6e2318304b8fd67e0282e4c9981d5bb90381b
Tim Giblin <timmyg13@gmail.com>
2016-10-21 10:30:36 -0400

    DE2056: fix some stuff maybe

--- front_end/src/app/admin/rooms/room.component.ts
+++ front_end/src/app/admin/rooms/room.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { FormControl, FormGroup } from '@angular/forms';
 import { AdminService } from '../admin.service';
@@ -9,31 +9,40 @@ import { Room } from './room';
   templateUrl: 'room.component.html',
   styleUrls: ['room.component.scss']
 })
-export class RoomComponent {
+export class RoomComponent implements OnInit {
   @Input() room: Room;
 
-  roomForm = new FormGroup({
-    Volunteers: new FormControl(),
-    Capacity: new FormControl(),
-    AllowSignIn: new FormControl()
-  });
+  private roomForm: FormGroup;
 
   constructor( private adminService: AdminService) {
-    this.roomForm.valueChanges
-      .debounceTime(1000)
-      .subscribe(props => {
-        this.adminService.updateRoom(this.room.EventId, this.room.RoomId, this.room).subscribe(val => {})
-      });
   }
 
   add(field): void {
     this.roomForm.controls[field].setValue(this.room[field]++);
   }
   remove(field): void {
-    this.roomForm.controls[field].setValue(this.room[field]--);
+    if(this.room[field] >= 1) {
+      this.roomForm.controls[field].setValue(this.room[field]--);
+    }
   }
   toggle(field): void {
     this.room[field] = !this.room[field]
     this.roomForm.controls[field].setValue(this.room[field]);
+  }
+
+  ngOnInit() {
+    this.roomForm = new FormGroup({
+      Volunteers: new FormControl(),
+      Capacity: new FormControl(),
+      AllowSignIn: new FormControl()
+    });
+
+    this.roomForm.valueChanges
+      .debounceTime(1000)
+      .subscribe(props => {
+        this.adminService.updateRoom(this.room.EventId, this.room.RoomId, this.room).subscribe(room => {
+          this.room = room;
+        })
+      });
   }
 }
