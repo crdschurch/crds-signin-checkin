710bef797db96adc7b8ec942372113323927ee91
Tim Giblin <timmyg13@gmail.com>
2016-10-28 15:36:04 -0400

    US5287: seems to be working besides token in body issue

--- front_end/src/app/shared/models/eventParticipants.ts
+++ front_end/src/app/shared/models/eventParticipants.ts
@@ -1,7 +1,23 @@
-import { Participant } from '.';
+import { Child } from './child';
 import { Event } from '../../admin/events/event';
 
 export class EventParticipants {
   CurrentEvent: Event;
-  Participants: Array<Participant>;
+  Participants: Array<Child>;
+
+  static fromJson(json: any): EventParticipants {
+    if (!json) {
+      return new EventParticipants();
+    }
+    console.log("json", json);
+
+    let eventParticipants = new EventParticipants();
+    eventParticipants.CurrentEvent = Event.fromJson(json.CurrentEvent);
+    eventParticipants.Participants = [];
+    for (let p of json.Participants) {
+      console.log(p);
+      eventParticipants.Participants.push(Child.fromJson(p));
+    }
+    return eventParticipants;
+  }
 }
