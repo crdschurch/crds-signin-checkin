55938819fed71b654f55ea2dc9f90b7e5fc511d9
Jim Kriz <jim.kriz@ingagepartners.com>
2016-10-31 10:24:08 -0400

    DE2150: Update to properly debounce API calls

--- front_end/src/app/admin/rooms/room-group.component.ts
+++ front_end/src/app/admin/rooms/room-group.component.ts
@@ -1,42 +1,22 @@
-import { Component, Input, EventEmitter } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { AdminService } from '../admin.service';
 import { Range } from './range';
 import { Group } from './group';
 import { Room } from './room';
-import { Observable } from 'rxjs/Rx';
 import * as _ from 'lodash';
 
 @Component({
   selector: '.group',
   templateUrl: 'room-group.component.html',
-  styleUrls: ['room-group.component.scss'],
-  providers: [ AdminService ]
+  styleUrls: ['room-group.component.scss']
 })
 export class RoomGroupComponent {
   @Input() group: Group;
   @Input() eventId: string;
   @Input() roomId: string;
   @Input() room: Room;
-  private updateEmitter: EventEmitter<Room>;
-  private updateObserver: Observable<Room>;
 
   constructor( private adminService: AdminService) {
-    // Create an emitter to use when sending updates to the room
-    this.updateEmitter = new EventEmitter<Room>();
-
-    // Setup an observer on the emitter, and set it to debounce for 2 seconds.
-    // This prevents the frontend from sending a backend update if multiple
-    // age ranges or grades are selected quickly.
-    this.updateObserver =
-      this.updateEmitter.map(room => room).debounceTime(2000);
-
-    // Subscribe to the debounced event - now actually send the update to
-    // the backend.
-    // TODO - Should handle the response, and notify the user of success or failure
-    // TODO - Should have some sort of processing state while the update is running, since it can take several seconds to complete 
-    this.updateObserver.subscribe(room => {
-      this.adminService.updateRoomGroups(room.EventId, room.RoomId, room);
-    });
   }
 
   toggleAll(group: Group) {
@@ -47,7 +27,7 @@ export class RoomGroupComponent {
           range.Selected = newState;
       }
     }
-    this.updateEmitter.emit(this.room);
+    this.adminService.updateRoomGroups(this.eventId, this.roomId, this.room);
   }
 
   toggleRange(range: Range, group: Group) {
@@ -63,7 +43,7 @@ export class RoomGroupComponent {
         group.Selected = true;
       }
     }
-    this.updateEmitter.emit(this.room);
+    this.adminService.updateRoomGroups(this.eventId, this.roomId, this.room);
   }
 
 }
