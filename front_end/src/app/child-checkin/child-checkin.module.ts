dd87c1950ab136d363a917b557ff50db1b01c58e
Tim Giblin <timmyg13@gmail.com>
2016-11-04 17:05:37 -0400

    US5295: events showing up in checkin screen, wip

--- front_end/src/app/child-checkin/child-checkin.module.ts
+++ front_end/src/app/child-checkin/child-checkin.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
+import { MomentModule } from 'angular2-moment';
 
+import { AdminService } from '../admin/admin.service';
+import { SetupService } from '../setup/setup.service';
 import { ChildCheckinComponent } from './child-checkin.component';
 import { RoomComponent } from './room';
 import { childCheckinRouting } from './child-checkin.routes';
@@ -12,8 +15,13 @@ import { childCheckinRouting } from './child-checkin.routes';
   ],
   imports: [
     SharedModule,
-    childCheckinRouting
+    childCheckinRouting,
+    MomentModule
   ],
+  providers: [
+    AdminService,
+    SetupService
+  ]
 })
 
 export class ChildCheckinModule { }
