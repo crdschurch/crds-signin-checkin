0e73017bb233b2f43e0ba89b7f6ae379fb88d696
Tim Giblin <timmyg13@gmail.com>
2016-11-07 16:23:00 -0500

    US5295: refactor tests

--- front_end/src/app/child-checkin/child-checkin.component.spec.ts
+++ front_end/src/app/child-checkin/child-checkin.component.spec.ts
@@ -5,20 +5,18 @@ import { MachineConfiguration } from '../setup/machine-configuration';
 import { Observable } from 'rxjs/Observable';
 
 let fixture: ChildCheckinComponent;
-let setupService: any;
-let adminService: any;
 let thisMachineConfig: MachineConfiguration;
-let event = { EventId: '123', IsCurrentEvent: false };
-let event2 = { EventId: '456', IsCurrentEvent: false };
-let eventCurrent = { EventId: '789', IsCurrentEvent: true };
+const event = { EventId: '123', IsCurrentEvent: false };
+const event2 = { EventId: '456', IsCurrentEvent: false };
+const eventCurrent = { EventId: '789', IsCurrentEvent: true };
+let setupService = jasmine.createSpyObj('setupService', ['getMachineDetailsConfigCookie']);
+setupService.getMachineDetailsConfigCookie.and.returnValue(thisMachineConfig);
+let adminService = jasmine.createSpyObj('adminService', ['getEvents']);
 
-fdescribe('Child Checkin Component', () => {
+describe('Child Checkin Component', () => {
   describe('#ngOnInit', () => {
     describe('initalization', () => {
       beforeEach(() => {
-        setupService = jasmine.createSpyObj('setupService', ['getMachineDetailsConfigCookie']);
-        setupService.getMachineDetailsConfigCookie.and.returnValue(thisMachineConfig);
-        adminService = jasmine.createSpyObj('adminService', ['getEvents']);
         adminService.getEvents.and.returnValue(Observable.of([{}]));
         fixture = new ChildCheckinComponent(setupService, adminService);
       });
@@ -31,31 +29,25 @@ fdescribe('Child Checkin Component', () => {
       });
     });
     describe('setting the current event', () => {
-      describe('should set where IsCurrentEvent is true', () => {
+      describe('and there is a current event', () => {
         beforeEach(() => {
-          setupService = jasmine.createSpyObj('setupService', ['getMachineDetailsConfigCookie']);
-          setupService.getMachineDetailsConfigCookie.and.returnValue(thisMachineConfig);
-          adminService = jasmine.createSpyObj('adminService', ['getEvents']);
           adminService.getEvents.and.returnValue(Observable.of([event, eventCurrent]));
           fixture = new ChildCheckinComponent(setupService, adminService);
         });
 
-        it('should set kiosk config details from cookie and get today\'s events', () => {
+        it('should set where IsCurrentEvent is true', () => {
           fixture.ngOnInit();
           expect(fixture.selectedEvent.EventId).toEqual(eventCurrent.EventId);
         });
       });
 
-      describe('should set first when no IsCurrentEvent', () => {
+      describe('and there is no current event', () => {
         beforeEach(() => {
-          setupService = jasmine.createSpyObj('setupService', ['getMachineDetailsConfigCookie']);
-          setupService.getMachineDetailsConfigCookie.and.returnValue(thisMachineConfig);
-          adminService = jasmine.createSpyObj('adminService', ['getEvents']);
           adminService.getEvents.and.returnValue(Observable.of([event, event2]));
           fixture = new ChildCheckinComponent(setupService, adminService);
         });
 
-        it('should set kiosk config details from cookie and get today\'s events', () => {
+        it('should set first when no IsCurrentEvent', () => {
           fixture.ngOnInit();
           expect(fixture.selectedEvent.EventId).toEqual(event.EventId);
         });
