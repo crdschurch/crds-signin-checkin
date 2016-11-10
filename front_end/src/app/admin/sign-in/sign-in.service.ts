6c00df1051ea4f93cd10247ed23c72d5709e599a
Jim Kriz <jim.kriz@ingagepartners.com>
2016-11-10 14:52:54 -0500

    Fix sign-in when error returned from login service

--- front_end/src/app/admin/sign-in/sign-in.service.ts
+++ front_end/src/app/admin/sign-in/sign-in.service.ts
@@ -15,7 +15,7 @@ export class SignInService {
   logIn(username: string, password: string) {
     let body = { username: username, password: password };
     return this.http.post(this.url, JSON.stringify(body))
-                    .map(res => res.json())
+                    .map(res => res)
                     .catch(this.handleError);
   }
 
@@ -23,6 +23,6 @@ export class SignInService {
     // in a real world app, we may send the error to some remote logging infrastructure
     // instead of just logging it to the console
     console.error(error);
-    return Observable.throw(error.json().error || 'Server error');
+    return Observable.throw(error || 'Server error');
   }
 }
