fa466e1f6ad778202a4c70790a14f04f69b63aed
jpc11623 <john.cleaver@ingagepartners.com>
2016-11-17 08:34:20 -0500

    Printing Labels
    US5288 - Fixing some unit tests

--- SignInCheckIn/Printing.Utilities/Services/Interfaces/IPrintingService.cs
+++ SignInCheckIn/Printing.Utilities/Services/Interfaces/IPrintingService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Printing.Utilities.Models;
+﻿using Printing.Utilities.Models;
 
 namespace Printing.Utilities.Services.Interfaces
 {
