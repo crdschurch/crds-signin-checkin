21d0f9211fcbfff898c85cab2c59379c9545cf4a
Jim Kriz <jim.kriz@ingagepartners.com>
2016-11-22 13:27:27 -0500

    DE2188: Reformat new hack method

--- front_end/src/app/app.component.ts
+++ front_end/src/app/app.component.ts
@@ -80,22 +80,22 @@ export class AppComponent implements OnInit {
   // https://github.com/valor-software/ng2-bootstrap/issues/986#issuecomment-262293199
   private hackToFixNg221BugForNg2Bootstrap(): void {
     ComponentsHelper.prototype.getRootViewContainerRef = function () {
-    // https://github.com/angular/angular/issues/9293
-    if (this.root) {
-      return this.root;
-    }
-    let comps = this.applicationRef.components;
-    if (!comps.length) {
-      throw new Error('ApplicationRef instance not found');
-    }
+      // https://github.com/angular/angular/issues/9293
+      if (this.root) {
+        return this.root;
+      }
+      let comps = this.applicationRef.components;
+      if (!comps.length) {
+        throw new Error('ApplicationRef instance not found');
+      }
       try {
-          /* one more ugly hack, read issue above for details */
-          let rootComponent = this.applicationRef._rootComponents[0];
-          // this.root = rootComponent._hostElement.vcRef;
-          this.root = rootComponent._component.viewContainerRef;
-          return this.root;
+        /* one more ugly hack, read issue above for details */
+        let rootComponent = this.applicationRef._rootComponents[0];
+        // this.root = rootComponent._hostElement.vcRef;
+        this.root = rootComponent._component.viewContainerRef;
+        return this.root;
       } catch (e) {
-          throw new Error('ApplicationRef instance not found');
+        throw new Error('ApplicationRef instance not found');
       }
     };
   }
