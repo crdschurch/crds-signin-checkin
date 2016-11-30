20a07e3a08dada6c087fec567a94278e70f077f2
dtkocher <dtkocher@gmail.com>
2016-11-30 17:54:31 -0500

    Get form working and displaying things right.

--- front_end/src/app/admin/events/new-family-registration/new-family-registration.component.ts
+++ front_end/src/app/admin/events/new-family-registration/new-family-registration.component.ts
@@ -4,6 +4,9 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { Event } from '../../../shared/models';
 import { ApiService } from '../../../shared/services';
 import { HeaderService } from '../../header/header.service';
+import { Child } from '../../../shared/models/child';
+
+import * as moment from 'moment';
 
 @Component({
   templateUrl: 'new-family-registration.component.html'
@@ -12,6 +15,7 @@ export class NewFamilyRegistrationComponent implements OnInit {
   private numberOfPossibleKids: Array<number> = [];
   private eventId: string;
   private event: Event;
+  private children: Array<Child> = [];
 
   constructor(
     private route: ActivatedRoute,
@@ -20,14 +24,40 @@ export class NewFamilyRegistrationComponent implements OnInit {
     private router: Router) {}
 
   ngOnInit() {
-    this.numberOfPossibleKids = Array(12).fill(12, 1, 12);
+    this.numberOfPossibleKids = Array.from({length: 12}, (v, k) => k + 1);
     this.eventId = this.route.snapshot.params['eventId'];
 
     this.apiService.getEvent(this.eventId).subscribe((event) => {
         this.event = event;
+        this.children = [this.newChild()];
         this.headerService.announceEvent(event);
       },
       error => console.error(error)
     );
+  }
+
+  updateNumberOfKids(numberOfKids: number): void {
+    let tmpChildren: Array<Child> = [];
+
+    for (let i = 0; i < numberOfKids; i++) {
+      if (this.children[i] === undefined) {
+        tmpChildren.push(this.newChild());
+      } else {
+        tmpChildren.push(this.children[i]);
+      }
+    }
+
+    this.children = tmpChildren;
+  }
+
+  needGradeLevel(child: Child): boolean {
+    return moment(child.DateOfBirth).isBefore(moment(this.event.EventStartDate).startOf('day').subtract(5, 'y'));
+  }
+
+  private newChild(): Child {
+    let child = new Child();
+    child.DateOfBirth = moment(this.event.EventStartDate).startOf('day').toDate();
+
+    return child;
   }
 }
