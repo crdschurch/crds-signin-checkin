24bcced0fff04d71136f247679c33323ce89fca0
Tim Giblin <timmyg13@gmail.com>
2016-12-02 10:41:44 -0500

    US5552: new child tests

---
+++ front_end/src/app/admin/events/new-family-registration/new-child/new-child.component.spec.ts
@@ -0,0 +1,20 @@
+import { NewChildComponent } from './new-child.component';
+import { NewChild } from '../../../../shared/models';
+import * as moment from 'moment';
+
+fdescribe('NewChildComponent', () => {
+  it('should return true when child > 5 years old', () => {
+    let fixture = new NewChildComponent();
+    let child = new NewChild();
+    child.DateOfBirth = moment().subtract(5, 'years').subtract(1, 'day').toDate();
+    console.log(child)
+    expect(fixture.needGradeLevel(child)).toBeTruthy();
+  });
+
+  it('should return false when child < 5 years old', () => {
+    let fixture = new NewChildComponent();
+    let child = new NewChild();
+    child.DateOfBirth = moment().subtract(5, 'years').add(1, 'day').toDate();
+    expect(fixture.needGradeLevel(child)).toBeFalsy();
+  });
+});
