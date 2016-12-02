828635e6185ba4b8de1bb8f8e416f60f56f523bd
Tim Giblin <timmyg13@gmail.com>
2016-12-02 13:16:53 -0500

    US5552: add tests

--- front_end/src/app/admin/events/new-family-registration/new-family-registration.component.spec.ts
+++ front_end/src/app/admin/events/new-family-registration/new-family-registration.component.spec.ts
@@ -1,7 +1,7 @@
 import { Observable } from 'rxjs';
 import { NewFamilyRegistrationComponent } from './new-family-registration.component';
 import { Router, ActivatedRoute, ActivatedRouteSnapshot } from '@angular/router';
-import { NewFamily, NewParent, NewChild } from '../../../../shared/models';
+import { NewFamily, NewParent, NewChild } from '../../../shared/models';
 import * as moment from 'moment';
 
 const event = {
@@ -13,20 +13,34 @@ let router;
 let apiService = jasmine.createSpyObj('apiService', ['getEvent']);
 let adminService = jasmine.createSpyObj('adminService', ['getGradeGroups']);
 let headerService = jasmine.createSpyObj('headerService', ['announceEvent']);
+let rootService;
 let route: ActivatedRoute;
 route = new ActivatedRoute();
 route.snapshot = new ActivatedRouteSnapshot();
 route.snapshot.params = { eventId: event.EventId };
+let fixture;
 
 fdescribe('NewFamilyRegistrationComponent', () => {
-  it('#ngOnInit', () => {
+  beforeEach(() => {
     (<jasmine.Spy>(apiService.getEvent)).and.returnValue(Observable.of(event));
     (<jasmine.Spy>(adminService.getGradeGroups)).and.returnValue(Observable.of());
-    let fixture = new NewFamilyRegistrationComponent(route, apiService, headerService, adminService, router);
+    fixture = new NewFamilyRegistrationComponent(route, apiService, headerService, adminService, rootService, router);
+  });
+  it('#ngOnInit', () => {
+    spyOn(fixture, 'setUp');
     fixture.ngOnInit();
+    expect(fixture.setUp).toHaveBeenCalled();
+  });
+  it('#setUp', () => {
+    fixture.setUp();
+    (<jasmine.Spy>(apiService.getEvent)).and.returnValue(Observable.of(event));
     expect(apiService.getEvent).toHaveBeenCalledWith(event.EventId);
-    expect(adminService.getGradeGroups).toHaveBeenCalledWith(event.EventId);
+    expect(adminService.getGradeGroups).toHaveBeenCalled();
     expect(headerService.announceEvent).toHaveBeenCalledWith(event);
   });
+  // it('#updateNumberOfKids', () => {
+  // });
+  // it('#onSubmit', () => {
+  // });
 
 });
