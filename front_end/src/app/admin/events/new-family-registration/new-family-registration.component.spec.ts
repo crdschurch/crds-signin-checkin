d3fcf9e0172ae1e9611fc6bff496866db2033d09
Tim Giblin <timmyg13@gmail.com>
2016-12-02 12:13:25 -0500

    US5552: test wip

--- front_end/src/app/admin/events/new-family-registration/new-family-registration.component.spec.ts
+++ front_end/src/app/admin/events/new-family-registration/new-family-registration.component.spec.ts
@@ -4,21 +4,29 @@ import { Router, ActivatedRoute, ActivatedRouteSnapshot } from '@angular/router'
 import { NewFamily, NewParent, NewChild } from '../../../../shared/models';
 import * as moment from 'moment';
 
-const eventId = '92398420398';
+const event = {
+  'EventTitle': 'Cool Event 83',
+  'EventId': '92398420398',
+};
+const eventTitle = 'Cool Event';
 let router;
 let apiService = jasmine.createSpyObj('apiService', ['getEvent']);
-let headerService;
+let adminService = jasmine.createSpyObj('adminService', ['getGradeGroups']);
+let headerService = jasmine.createSpyObj('headerService', ['announceEvent']);
 let route: ActivatedRoute;
 route = new ActivatedRoute();
 route.snapshot = new ActivatedRouteSnapshot();
-route.snapshot.params = { eventId: eventId };
+route.snapshot.params = { eventId: event.EventId };
 
 fdescribe('NewFamilyRegistrationComponent', () => {
   it('#ngOnInit', () => {
-    (<jasmine.Spy>(apiService.getEvent)).and.returnValue(Observable.of([{}]));
-    let fixture = new NewFamilyRegistrationComponent(route, apiService, headerService, router);
+    (<jasmine.Spy>(apiService.getEvent)).and.returnValue(Observable.of(event));
+    (<jasmine.Spy>(adminService.getGradeGroups)).and.returnValue(Observable.of());
+    let fixture = new NewFamilyRegistrationComponent(route, apiService, headerService, adminService, router);
     fixture.ngOnInit();
-    expect(apiService.getEvent).toHaveBeenCalledWith(eventId);
+    expect(apiService.getEvent).toHaveBeenCalledWith(event.EventId);
+    expect(adminService.getGradeGroups).toHaveBeenCalledWith(event.EventId);
+    expect(headerService.announceEvent).toHaveBeenCalledWith(event);
   });
 
 });
