1389f9a29d8e2ada0931ed03f7ab15a73a8f8570
dtkocher <dtkocher@gmail.com>
2016-12-05 08:50:27 -0500

    Remove dead components.

--- front_end/src/app/admin/events/new-family-registration/new-family-registration.module.ts
+++ front_end/src/app/admin/events/new-family-registration/new-family-registration.module.ts
@@ -4,15 +4,11 @@ import { MomentModule } from 'angular2-moment';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { HeaderModule } from '../../header';
-import { NewParentComponent } from './new-parent/new-parent.component';
-import { NewChildComponent } from './new-child/new-child.component';
 import { NewFamilyRegistrationComponent } from './new-family-registration.component';
 import { newFamilyRegistrationRouting } from './new-family-registration.routes';
 
 @NgModule({
   declarations: [
-    NewParentComponent,
-    NewChildComponent,
     NewFamilyRegistrationComponent,
   ],
   imports: [
