59eb7524f1a7036b245c45c1c8a71ad53063f462
Tim Giblin <timmyg13@gmail.com>
2016-12-14 09:51:48 -0500

    US5985: adventure club on off logic

--- front_end/src/app/admin/events/rooms/room-group-list.component.ts
+++ front_end/src/app/admin/events/rooms/room-group-list.component.ts
@@ -4,7 +4,8 @@ import { Location } from '@angular/common';
 import { HeaderService } from '../../header/header.service';
 import { Group, Room, Event } from '../../../shared/models';
 import { AdminService } from '../../admin.service';
-import { ApiService } from '../../../shared/services';
+import { ApiService, RootService } from '../../../shared/services';
+import * as _ from 'lodash';
 
 @Component({
   templateUrl: 'room-group-list.component.html',
@@ -22,6 +23,7 @@ export class RoomGroupListComponent implements OnInit {
 
   constructor( private apiService: ApiService,
                private adminService: AdminService,
+               private rootService: RootService,
                private route: ActivatedRoute,
                private headerService: HeaderService,
                private location: Location) {
@@ -61,6 +63,15 @@ export class RoomGroupListComponent implements OnInit {
     return this.isReady() ? this.event : new Event();
   }
 
+  hasActiveRooms(): boolean {
+    for (let group of this.room.AssignedGroups) {
+        if (_.some(group.Ranges, { 'Selected': true})) {
+          return true;
+        }
+    }
+    return false;
+  }
+
   getGroups(): Group[] {
     return this.isReady() ? this.room.AssignedGroups : [];
   }
@@ -80,6 +91,18 @@ export class RoomGroupListComponent implements OnInit {
   openTabIfAlternateRoomsHash() {
     if (this.route.snapshot.queryParams['tab'] === 'alternate-rooms') {
       this.alternateRoomsSelected = true;
+    }
+  }
+
+  toggleAdventureClub(e) {
+    // if has rooms, dont change toggle, show alert
+    if (this.hasActiveRooms()) {
+      this.isAdventureClub = !this.isAdventureClub;
+      e.target.checked = this.isAdventureClub;
+      this.rootService.announceEvent('echeckAdventureClubToggleWarning');
+    } else {
+      // if turning on, call PUT /event/123/adventureclub
+      // else if turning off, DELETE /event/123/adventureclub
     }
   }
 
