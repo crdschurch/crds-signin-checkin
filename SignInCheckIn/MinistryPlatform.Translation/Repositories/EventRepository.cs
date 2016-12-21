3a00111b49f65663f560fe43b2916d0bf5ff4276
Tim <timmyg13@gmail.com>
2016-12-21 13:33:17 -0500

    US3527: include subevents optional param in getEvents

--- SignInCheckIn/MinistryPlatform.Translation/Repositories/EventRepository.cs
+++ SignInCheckIn/MinistryPlatform.Translation/Repositories/EventRepository.cs
@@ -57,15 +57,23 @@ namespace MinistryPlatform.Translation.Repositories
             };
         }
 
-        public List<MpEventDto> GetEvents(DateTime startDate, DateTime endDate, int site)
+        public List<MpEventDto> GetEvents(DateTime startDate, DateTime endDate, int site, bool? includeSubevents = false)
         {
             var apiUserToken = _apiUserRepository.GetToken();
 
             var startTimeString = startDate.ToString();
             // make sure end time is end of day
             var endTimeString = endDate.AddHours(23).AddMinutes(59).AddSeconds(59).ToString();
+
+            var queryString =
+                $"[Allow_Check-in]=1 AND [Cancelled]=0 AND [Event_Start_Date] >= '{startTimeString}' AND [Event_Start_Date] <= '{endTimeString}' AND Events.[Congregation_ID] = {site}";
+            if (includeSubevents != true)
+            {
+                // do not include subevents
+                queryString = $"{queryString} AND[Parent_Event_ID] IS NULL";
+            }
             return _ministryPlatformRestRepository.UsingAuthenticationToken(apiUserToken)
-                .Search<MpEventDto>($"[Allow_Check-in]=1 AND [Cancelled]=0 AND [Parent_Event_ID] IS NULL AND [Event_Start_Date] >= '{startTimeString}' AND [Event_Start_Date] <= '{endTimeString}' AND Events.[Congregation_ID] = {site}", _eventColumns);
+                .Search<MpEventDto>(queryString, _eventColumns);
         }
 
         public MpEventDto GetEventById(int eventId)
