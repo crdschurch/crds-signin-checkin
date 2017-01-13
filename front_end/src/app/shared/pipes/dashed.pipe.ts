c86086692a4a6676d798082b4e6d7a4fdee8e161
Tim Giblin <timmyg13@gmail.com>
2017-01-13 13:42:43 -0500

    US3652: dash pipe tests

--- front_end/src/app/shared/pipes/dashed.pipe.ts
+++ front_end/src/app/shared/pipes/dashed.pipe.ts
@@ -6,7 +6,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 export class DashedPipe implements PipeTransform {
   transform(val: number, length = 6) {
-    const callNumMasked = `${val}————`;
+    let dashes = '';
+    let i = 0;
+    while (i < length) {
+      dashes = `${dashes}—`;
+      i++;
+    }
+    const callNumMasked = `${val}${dashes}`;
     return callNumMasked.slice(0, length);
   }
 }
