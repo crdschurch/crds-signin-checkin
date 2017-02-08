bbe4be8992fa16b8c3c34d3c857cd17f672af272
dtkoc <dtkocher@gmail.com>
2017-02-08 08:49:43 -0500

    Initial setup of SignalR.

--- SignInCheckIn/SignInCheckIn/Startup.cs
+++ SignInCheckIn/SignInCheckIn/Startup.cs
@@ -10,6 +10,7 @@ namespace SignInCheckIn
         public void Configuration(IAppBuilder app)
         {
             // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            app.MapSignalR();
         }
     }
 }
