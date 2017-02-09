e3a67143bc89624adf8560e334632f982120e0a0
dtkoc <dtkocher@gmail.com>
2017-02-09 13:05:56 -0500

    Clean up the signalr mapping

--- SignInCheckIn/SignInCheckIn/Startup.cs
+++ SignInCheckIn/SignInCheckIn/Startup.cs
@@ -12,8 +12,7 @@ namespace SignInCheckIn
         public void Configuration(IAppBuilder app)
         {
             // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
-
-            // Branch the pipeline here for requests that start with "/signalr"
+            // Branch the pipeline here for requests that start with /signalr
             app.Map("/signalr", map =>
             {
                 // Setup the CORS middleware to run before SignalR.
@@ -21,17 +20,11 @@ namespace SignInCheckIn
                 // configure the set of origins and/or http verbs by
                 // providing a cors options with a different policy.
                 map.UseCors(CorsOptions.AllowAll);
-                var hubConfiguration = new HubConfiguration
-                {
-                    // You can enable JSONP by uncommenting line below.
-                    // JSONP requests are insecure but some older browsers (and some
-                    // versions of IE) require JSONP to work cross domain
-                    // EnableJSONP = true
-                };
+
                 // Run the SignalR pipeline. We're not using MapSignalR
-                // since this branch already runs under the "/signalr"
+                // since this branch already runs under the /signalr
                 // path.
-                map.RunSignalR(hubConfiguration);
+                map.RunSignalR();
             });
         }
     }
