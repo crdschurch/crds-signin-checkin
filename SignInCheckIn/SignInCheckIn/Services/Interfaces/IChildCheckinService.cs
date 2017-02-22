86c83ea3468ecbd963d64cebf2aa9ef8a1950677
dtkoc <dtkocher@gmail.com>
2017-02-22 15:27:30 -0500

    Fix Bugs

--- SignInCheckIn/SignInCheckIn/Services/Interfaces/IChildCheckinService.cs
+++ SignInCheckIn/SignInCheckIn/Services/Interfaces/IChildCheckinService.cs
@@ -7,7 +7,7 @@ namespace SignInCheckIn.Services.Interfaces
     public interface IChildCheckinService
     {
         ParticipantEventMapDto GetChildrenForCurrentEventAndRoom(int roomId, int siteId, int? eventId);
-        List<ParticipantDto> GetChildrenForCurrentEventAndRoom(int roomId, int eventId)
+        List<ParticipantDto> GetChildrenForCurrentEventAndRoom(int roomId, int eventId);
         ParticipantDto CheckinChildrenForCurrentEventAndRoom(ParticipantDto eventParticipant);
         ParticipantDto GetEventParticipantByCallNumber(int eventId, int callNumber, int roomId, bool? excludeThisRoom = false);
         bool OverrideChildIntoRoom(int eventId, int eventParticipantId, int roomId);
