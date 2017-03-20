6c4db9df0783b7f6643be3f13010ecf4ef469c20
Tim <timmyg13@gmail.com>
2017-03-20 17:36:02 -0400

    US6303:  Congregation repo wip

---
+++ SignInCheckIn/MinistryPlatform.Translation/Repositories/Interfaces/ISiteRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using MinistryPlatform.Translation.Models.DTO;
+using Newtonsoft.Json.Linq;
+
+namespace MinistryPlatform.Translation.Repositories.Interfaces
+{
+    public interface ISiteRepository
+    {
+        List<MpCongregationDto> GetAll();
+    }
+}
---
+++ SignInCheckIn/SignInCheckIn/Controllers/SiteController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Http.Description;
+using MinistryPlatform.Translation.Repositories.Interfaces;
+using SignInCheckIn.Exceptions.Models;
+using SignInCheckIn.Models.DTO;
+using SignInCheckIn.Security;
+using SignInCheckIn.Services.Interfaces;
+using Crossroads.ApiVersioning;
+using Crossroads.Web.Common.Security;
+using MinistryPlatform.Translation.Models.DTO;
+
+namespace SignInCheckIn.Controllers
+{
+    public class SiteController : ApiController
+    {
+        private readonly ISiteRepository _siteRepository;
+
+        public SiteController(ISiteRepository siteRepository)
+        {
+            _siteRepository = siteRepository;
+        }
+
+        [HttpGet]
+        [ResponseType(typeof(List<MpCongregationDto>))]
+        [VersionedRoute(template: "sites", minimumVersion: "1.0.0")]
+        [Route("sites")]
+        public IHttpActionResult GetSites()
+        {
+            return Ok(_siteRepository.GetAll());
+        }
+    }
+}
