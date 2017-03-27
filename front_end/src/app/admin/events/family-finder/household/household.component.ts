cfd492ad28bc60a370e035c3ff561c42fe5145f4
Tim Giblin <timmyg13@gmail.com>
2017-03-27 16:32:08 -0400

    feature/family-finder: add household route and boilerplate

---
+++ front_end/src/app/admin/events/family-finder/household/household.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../../../../shared/services';
+import { HeaderService } from '../../../header/header.service';
+
+@Component({
+  selector: 'household',
+  templateUrl: 'household.component.html',
+  styleUrls: ['household.component.scss']
+})
+export class HouseholdComponent implements OnInit {
+  private eventId: string;
+  private processing: boolean;
+
+  constructor( private apiService: ApiService,
+               private route: ActivatedRoute,
+               private headerService: HeaderService) {}
+
+ ngOnInit() {
+   this.processing = false;
+   this.eventId = this.route.snapshot.params['eventId'];
+
+   this.apiService.getEvent(this.eventId).subscribe((event) => {
+     this.headerService.announceEvent(event);
+   }, error => console.error(error));
+ }
+
+}
