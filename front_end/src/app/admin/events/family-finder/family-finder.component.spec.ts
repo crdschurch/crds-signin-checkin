0ae8a76e07cbc7a7fece1571c43b97cb373ff3d0
dtkocher <dtkocher@gmail.com>
2017-03-28 10:53:02 -0400

    Finish up the front end and tests.

--- front_end/src/app/admin/events/family-finder/family-finder.component.spec.ts
+++ front_end/src/app/admin/events/family-finder/family-finder.component.spec.ts
@@ -6,12 +6,11 @@ const event = {
   'EventTitle': 'Cool Event 83',
   'EventId': '92398420398',
 };
-let router;
-let apiService = jasmine.createSpyObj('apiService', ['getEvent', 'getGradeGroups']);
-let adminService = jasmine.createSpyObj('adminService', [, 'createNewFamily']);
+
+let apiService = jasmine.createSpyObj('apiService', ['getEvent']);
+let adminService = jasmine.createSpyObj('adminService', [, 'findFamilies']);
 let headerService = jasmine.createSpyObj('headerService', ['announceEvent']);
-let route: ActivatedRoute;
-route = new ActivatedRoute();
+let route: ActivatedRoute = new ActivatedRoute();
 route.snapshot = new ActivatedRouteSnapshot();
 route.snapshot.params = { eventId: event.EventId };
 let fixture;
@@ -19,11 +18,37 @@ let fixture;
 describe('FamilyFinderComponent', () => {
   beforeEach(() => {
     (<jasmine.Spy>(apiService.getEvent)).and.returnValue(Observable.of(event));
-    (<jasmine.Spy>(apiService.getGradeGroups)).and.returnValue(Observable.of());
-    (<jasmine.Spy>(adminService.createNewFamily)).and.returnValue(Observable.of());
-    fixture = new FamilyFinderComponent(route, apiService, headerService, adminService);
+    (<jasmine.Spy>(adminService.findFamilies)).and.returnValue(Observable.of());
+    fixture = new FamilyFinderComponent(route, adminService, headerService, apiService);
   });
+
   it('#ngOnInit', () => {
     fixture.ngOnInit();
+    expect(fixture.processing).toBeFalsy;
+    expect(fixture.searched).toBeFalsy;
+    expect(apiService.getEvent).toHaveBeenCalledWith(event.EventId);
+    expect(headerService.announceEvent).toHaveBeenCalledWith(event);
+  });
+
+  it('#setSearchValue(search)', () => {
+    fixture.setSearchValue('hi');
+    expect(fixture.search).toEqual('hi');
+  });
+
+  it('#onClearSearch(box)', () => {
+    fixture.search = 'hi';
+    expect(fixture.search).toEqual('hi');
+
+    fixture.onClearSearch({value: 'hello'});
+    expect(fixture.search).toEqual('');
+    expect(fixture.contacts).toEqual([]);
+    expect(fixture.searched).toBeFalsy;
+  });
+
+  it('#onSearch()', () => {
+    fixture.onSearch();
+    expect(fixture.processing).toBeTruthy;
+    expect(fixture.searched).toBeTruthy;
+    expect(adminService.findFamilies).toHaveBeenCalledWith(fixture.search);
   });
 });
