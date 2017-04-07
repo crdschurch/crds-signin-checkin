fc9a6bfbea30624112467babd40f38c24977bda4
dtkoc <dtkocher@gmail.com>
2017-04-07 11:27:11 -0400

    Add new lookup controller

--- SignInCheckIn/SignInCheckIn/Controllers/EventController.cs
+++ SignInCheckIn/SignInCheckIn/Controllers/EventController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Web.Http;
 using System.Web.Http.Description;
-using MinistryPlatform.Translation.Repositories.Interfaces;
 using SignInCheckIn.Exceptions.Models;
 using SignInCheckIn.Models.DTO;
 using SignInCheckIn.Security;
@@ -263,42 +262,6 @@ namespace SignInCheckIn.Controllers
             catch (Exception e)
             {
                 var apiError = new ApiErrorDto($"Error updating Household ID of {householdDto.HouseholdId}", e);
-                throw new HttpResponseException(apiError.HttpResponseMessage);
-            }
-        }
-
-        [HttpGet]
-        [ResponseType(typeof(HouseholdDto))]
-        [VersionedRoute(template: "geStates", minimumVersion: "1.0.0")]
-        [Route("geStates")]
-        public IHttpActionResult GeStates()
-        {
-            try
-            {
-
-                return Authorized(token => Ok());
-            }
-            catch (Exception e)
-            {
-                var apiError = new ApiErrorDto($"Error getting states", e);
-                throw new HttpResponseException(apiError.HttpResponseMessage);
-            }
-        }
-
-        [HttpGet]
-        [ResponseType(typeof(HouseholdDto))]
-        [VersionedRoute(template: "getCountries", minimumVersion: "1.0.0")]
-        [Route("getCountries")]
-        public IHttpActionResult GetCountries()
-        {
-            try
-            {
-
-                return Authorized(token => Ok());
-            }
-            catch (Exception e)
-            {
-                var apiError = new ApiErrorDto($"Error getting countries", e);
                 throw new HttpResponseException(apiError.HttpResponseMessage);
             }
         }
---
+++ SignInCheckIn/SignInCheckIn/Controllers/LookupController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Http.Description;
+using MinistryPlatform.Translation.Repositories.Interfaces;
+using SignInCheckIn.Exceptions.Models;
+using SignInCheckIn.Models.DTO;
+using SignInCheckIn.Security;
+using SignInCheckIn.Services.Interfaces;
+using Crossroads.ApiVersioning;
+using Crossroads.Web.Common.Security;
+
+namespace SignInCheckIn.Controllers
+{
+    public class LookupController : MpAuth
+    {
+        private readonly ILookupService _lookupService;
+
+        public LookupController(ILookupService lookupService, IAuthenticationRepository authenticationRepository) : base(authenticationRepository)
+        {
+            _lookupService = lookupService;
+        }
+
+        [HttpGet]
+        [ResponseType(typeof(StateDto))]
+        [VersionedRoute(template: "geStates", minimumVersion: "1.0.0")]
+        [Route("geStates")]
+        public IHttpActionResult GeStates()
+        {
+            try
+            {
+                return Authorized(token => Ok(_lookupService.GetStates()));
+            }
+            catch (Exception e)
+            {
+                var apiError = new ApiErrorDto($"Error getting states", e);
+                throw new HttpResponseException(apiError.HttpResponseMessage);
+            }
+        }
+
+        [HttpGet]
+        [ResponseType(typeof(HouseholdDto))]
+        [VersionedRoute(template: "getCountries", minimumVersion: "1.0.0")]
+        [Route("getCountries")]
+        public IHttpActionResult GetCountries()
+        {
+            try
+            {
+                return Authorized(token => Ok(_lookupService.GetCountries()));
+            }
+            catch (Exception e)
+            {
+                var apiError = new ApiErrorDto($"Error getting countries", e);
+                throw new HttpResponseException(apiError.HttpResponseMessage);
+            }
+        }
+    }
+}
