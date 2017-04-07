81c3c12439122ae18c0518ec603792c72cce4e68
dtkoc <dtkocher@gmail.com>
2017-04-07 11:20:34 -0400

    Add Lookup Service to get Country and State list from MP

---
+++ SignInCheckIn/SignInCheckIn/Services/Interfaces/ILookupService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using SignInCheckIn.Models.DTO;
+
+namespace SignInCheckIn.Services.Interfaces
+{
+    public interface ILookupService
+    {
+        List<StateDto> GetStates();
+        List<CountryDto> GetCountries();
+    }
+}
