67a533459a8863050dd4846db266037e078b6c10
Tim <timmyg13@gmail.com>
2017-04-11 15:59:33 -0400

    US7539: fix tests

--- SignInCheckIn/SignInCheckIn.Tests/Services/ChildSigninServiceTest.cs
+++ SignInCheckIn/SignInCheckIn.Tests/Services/ChildSigninServiceTest.cs
@@ -897,7 +897,7 @@ namespace SignInCheckIn.Tests.Services
             MpNewParticipantDto newParticipantDto = new MpNewParticipantDto();
 
             _contactRepository.Setup(m => m.CreateHousehold(token, It.IsAny<MpHouseholdDto>())).Returns(mpHouseholdDto);
-            _participantRepository.Setup(m => m.CreateParticipantWithContact(It.IsAny<MpNewParticipantDto>())).Returns(newParticipantDto);
+            _participantRepository.Setup(m => m.CreateParticipantWithContact(It.IsAny<MpNewParticipantDto>(), It.IsAny<string>())).Returns(newParticipantDto);
 
             // Act
             var result = _fixture.SaveNewFamilyData(token, newFamilyDto);
@@ -939,7 +939,7 @@ namespace SignInCheckIn.Tests.Services
                 ParticipantId = participantId
             };
 
-            _participantRepository.Setup(m => m.CreateParticipantWithContact(It.IsAny<MpNewParticipantDto>())).Returns(mpNewParticipantDto);
+            _participantRepository.Setup(m => m.CreateParticipantWithContact(It.IsAny<MpNewParticipantDto>(), It.IsAny<string>())).Returns(mpNewParticipantDto);
 
             _groupLookupRepository.Setup(m => m.GetGroupId(new DateTime(2008, 10, 10), null)).Returns(groupId);
 
@@ -1009,7 +1009,7 @@ namespace SignInCheckIn.Tests.Services
                 ParticipantId = participantId
             };
 
-            _participantRepository.Setup(m => m.CreateParticipantWithContact(It.IsAny<MpNewParticipantDto>())).Returns(mpNewParticipantDto);
+            _participantRepository.Setup(m => m.CreateParticipantWithContact(It.IsAny<MpNewParticipantDto>(), It.IsAny<string>())).Returns(mpNewParticipantDto);
 
             _groupLookupRepository.Setup(m => m.GetGroupId(new DateTime(2008, 10, 10), null)).Returns(groupId);
 
