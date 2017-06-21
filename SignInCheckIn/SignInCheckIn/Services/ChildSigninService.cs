e7bd6ce7247d733bbb8e119a42a4fc6c68a63a32
jpc11623 <john.cleaver@ingagepartners.com>
2017-06-21 06:31:02 -0400

    DE3705 - Do not create user no email address
    Fixing a broken window

--- SignInCheckIn/SignInCheckIn/Services/ChildSigninService.cs
+++ SignInCheckIn/SignInCheckIn/Services/ChildSigninService.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Permissions;
 using AutoMapper;
 using Crossroads.Utilities.Services.Interfaces;
 using MinistryPlatform.Translation.Models;
