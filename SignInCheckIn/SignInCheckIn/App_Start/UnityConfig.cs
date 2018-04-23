97ee8814f307e94eba1ca2ad0171d8c5cf799ed7
jpc11623 <john.cleaver@ingagepartners.com>
2018-04-23 09:34:36 -0400

    US12575 - upgrade auth

--- SignInCheckIn/SignInCheckIn/App_Start/UnityConfig.cs
+++ SignInCheckIn/SignInCheckIn/App_Start/UnityConfig.cs
@@ -4,6 +4,7 @@ using Microsoft.Practices.Unity;
 using System.Web.Http;
 using Microsoft.Practices.Unity.Configuration;
 using Unity.WebApi;
+using Crossroads.Web.Common.Configuration;
 
 namespace SignInCheckIn
 {
@@ -11,8 +12,19 @@ namespace SignInCheckIn
     {
         public static void RegisterComponents()
         {
+            //var container = new UnityContainer();
+            //var unitySections = new[] { "crossroadsCommonUnity", "crossroadsClientApiKeysUnity", "unity" };
+
+            //foreach (var section in unitySections.Select(sectionName => (UnityConfigurationSection)ConfigurationManager.GetSection(sectionName)))
+            //{
+            //    container.LoadConfiguration(section);
+            //}
+
+            //GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
+
             var container = new UnityContainer();
-            var unitySections = new[] { "crossroadsCommonUnity", "crossroadsClientApiKeysUnity", "unity" };
+            CrossroadsWebCommonConfig.Register(container);
+            var unitySections = new[] { "crossroadsCommonUnity", "unity" };
 
             foreach (var section in unitySections.Select(sectionName => (UnityConfigurationSection)ConfigurationManager.GetSection(sectionName)))
             {
