9ca157386d1506050461a686d4948c323723e807
jpc11623 <john.cleaver@ingagepartners.com>
2018-05-03 09:48:59 -0400

    US12575 - Ouath upgrade
    Now with working!

--- SignInCheckIn/SignInCheckIn/App_Start/UnityConfig.cs
+++ SignInCheckIn/SignInCheckIn/App_Start/UnityConfig.cs
@@ -16,53 +16,17 @@ namespace SignInCheckIn
     {
         public static void RegisterComponents()
         {
+            // this register unity config content that was previous registered in the unity sections area
             var container = new UnityContainer();
-            //var unitySections = new[] { "crossroadsCommonUnity", "crossroadsClientApiKeysUnity", "unity" };
-
-            //foreach (var section in unitySections.Select(sectionName => (UnityConfigurationSection)ConfigurationManager.GetSection(sectionName)))
-            //{
-            //    container.LoadConfiguration(section);
-            //}
-
-            //GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
-
-            //var container = new Microsoft.Practices.Unity.UnityContainer();
             CrossroadsWebCommonConfig.Register(container);
 
-            //var container = new UnityContainer();
-            //CrossroadsWebCommonConfig.Register(container);
-            //var unitySections = new[] { "crossroadsCommonUnity", "unity" };
+            var unitySections = new[] { "unity" };
 
-            //foreach (var section in unitySections.Select(sectionName => (UnityConfigurationSection)ConfigurationManager.GetSection(sectionName)))
-            //{
-            //    container.LoadConfiguration(section);
-            //}
+            foreach (var section in unitySections.Select(sectionName => (UnityConfigurationSection)ConfigurationManager.GetSection(sectionName)))
+            {
+                container.LoadConfiguration(section);
+            }
 
-            container.RegisterType<IAttributeRepository, AttributeRepository>();
-            container.RegisterType<IChildCheckinRepository, ChildCheckinRepository>();
-            container.RegisterType<IChildSigninRepository, ChildSigninRepository>();
-            container.RegisterType<IConfigRepository, ConfigRepository>();
-            container.RegisterType<IContactRepository, ContactRepository>();
-            container.RegisterType<IEventRepository, EventRepository>();
-            container.RegisterType<IGroupLookupRepository, GroupLookupRepository>();
-            container.RegisterType<IGroupRepository, GroupRepository>();
-            container.RegisterType<IKioskRepository, KioskRepository>();
-            container.RegisterType<ILookupRepository, LookupRepository>();
-            container.RegisterType<IParticipantRepository, ParticipantRepository>();
-            container.RegisterType<IRoomRepository, RoomRepository>();
-            container.RegisterType<ISiteRepository, SiteRepository>();
-
-            container.RegisterType<IChildCheckinService, ChildCheckinService>();
-            container.RegisterType<IChildSigninService, ChildSigninService>();
-            container.RegisterType<IEventService, EventService>();
-            container.RegisterType<IFamilyService, FamilyService>();
-            container.RegisterType<IHelloWorldService, HelloWorldService>();
-            container.RegisterType<IKioskService, KioskService>();
-            container.RegisterType<ILoginService, LoginService>();
-            container.RegisterType<ILookupService, LookupService>();
-            container.RegisterType<IRoomService, RoomService>();
-            container.RegisterType<ISiteService, SiteService>();
-            
             GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
         }
     }
