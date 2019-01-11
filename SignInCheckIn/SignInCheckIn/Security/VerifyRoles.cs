7b575ff01305dfe5f1210c4d0e12e42a5a21de7f
candrews-thrive <22330518+candrews55@users.noreply.github.com>
2019-01-11 12:00:23 -0500

    Add a verification class

---
+++ SignInCheckIn/SignInCheckIn/Security/VerifyRoles.cs
@@ -0,0 +1,17 @@
+﻿using Crossroads.Web.Auth.Models;
+using System;
+
+namespace SignInCheckIn.Security
+{
+    public static class VerifyRoles
+    {
+        public static void KidsClubTools(AuthDTO authDto)
+        {
+            const int KidsClubTools = 112;
+            if (!authDto.Authorization.MpRoles.ContainsKey(KidsClubTools))
+            {
+                throw new UnauthorizedAccessException();
+            }
+        }
+    }
+}
